4be6633115fc611bcb2f87c0def3a1e8d28cb9ae
Cristhofer Pincetti <github.annotate387@passmail.net>
2025-09-16 17:14:09 -0300

    refactor: change Card component to named export

--- src/features/shared/components/card/card.tsx
+++ src/features/shared/components/card/card.tsx
@@ -5,7 +5,7 @@ export interface CardProps {
 	children: React.ReactNode;
 }
 
-export default function Card({ className, children }: CardProps) {
+export function Card({ className, children }: CardProps) {
 	return (
 		<div className={`bg-card text-card-foreground rounded-lg border p-6 shadow-sm ${className}`}>
 			{children}
